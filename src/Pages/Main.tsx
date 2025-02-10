6d3c837440f74411c87f3e6041e4710c0aadf09e
Nikhil Hazari <nikhilhazari100@gmail.com>
2025-02-11 00:01:22 +0530

    3rd half started

--- src/Pages/Main.tsx
+++ src/Pages/Main.tsx
@@ -2,13 +2,20 @@
 import {getDocs, collection} from 'firebase/firestore'
 import { db } from '../config/firebase';
 import { useState } from 'react';
+interface Post{
+  id:string,
+  userId:string;
+  title:string;
+  username:string;
+  description:string;
+}
 export const Main = () => {
-  const [ postsList,SetpostsList] = useState(null)
+  const [ postsList,SetpostsList] = useState<Post[]| null>(null)
   const postsRef = collection(db,"posts");
   const getPosts= async()=>{
 const data = await getDocs(postsRef);
-console.log(data.docs.map((doc)=>({...doc.data(), id:doc.id})));
-  }
+SetpostsList(data.docs.map((doc)=>({...doc.data(), id:doc.id})));
+  } 
   getPosts();
   return (
     <div>Main Page</div>
