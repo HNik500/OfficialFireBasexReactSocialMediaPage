b4e2f612d58834018227593e4e0321f606aa3417
Nikhil Hazari <nikhilhazari100@gmail.com>
2025-02-10 23:50:00 +0530

    1st

--- src/Pages/Main.tsx
+++ src/Pages/Main.tsx
@@ -1,7 +1,9 @@
 //  import React from 'react'
 import {getDocs, collection} from 'firebase/firestore'
 import { db } from '../config/firebase';
+import { useState } from 'react';
 export const Main = () => {
+  const [ postsList,SetpostsList] = useState(null)
   const postsRef = collection(db,"posts");
   return (
     <div>Main Page</div>
