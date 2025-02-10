a7a21efd4b2e1b052fa3940df0d4aed5220d31c6
Nikhil Hazari <nikhilhazari100@gmail.com>
2025-02-10 23:47:33 +0530

    1st

--- src/Pages/Main.tsx
+++ src/Pages/Main.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
-
+//  import React from 'react'
+import {getDocs, collection} from 'firebase/firestore'
+import { db } from '../config/firebase';
 export const Main = () => {
+  const postsRef = collection(db,"posts");
   return (
     <div>Main Page</div>
   )
