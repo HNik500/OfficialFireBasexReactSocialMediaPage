34c18142f8d0d5bbfd23d4677a2768fd9bf996ca
Nikhil Hazari <nikhilhazari100@gmail.com>
2025-02-15 23:59:00 +0530

    3rd half started

--- src/Pages/Main/Post.tsx
+++ src/Pages/Main/Post.tsx
@@ -8,6 +8,9 @@ export const Post =(props:Props)=>{
     const {post}=props;
     return <div>hi from Post
 
-        <div className="title"></div>
+        <div className="title">
+            <h1 className="title">{post.title}</h1>
+        </div>
+        <div></div>
     </div>
 }
