c43a1294798d265851f3aed711e9086a254fc528
Nikhil Hazari <nikhilhazari100@gmail.com>
2025-02-16 23:59:14 +0530

    started mini

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -11,7 +11,7 @@ export const Login =  () => {
         navigate("/")
     }
   return (
-    <div><p>Sign in with google too continue</p>
+    <div><p>Sign in with google to continue</p>
     <button onClick={signInWithGoogle}>Sign in with Google</button>
     </div>
   )
