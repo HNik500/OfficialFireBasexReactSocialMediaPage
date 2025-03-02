9757a8a5937ad7eb74005f01b5fba8983ce1e67a
Nikhil Hazari <nikhilhazari100@gmail.com>
2025-03-02 23:24:55 +0530

    trying like feature

--- src/Pages/Main/Post.tsx
+++ src/Pages/Main/Post.tsx
@@ -1,36 +1,43 @@
 import { addDoc, collection } from "firebase/firestore";
-import { Post as IPost } from "./Main"
+import { Post as IPost } from "./Main";
 import { auth, db } from "../../config/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 
-interface Props{
+interface Props {
     post: IPost;
 }
 
-export const Post =(props:Props)=>{
-    const {post}=props;
-    const [user]=useAuthState(auth);
-      const LikesRef = collection(db,"likes")
+export const Post = (props: Props) => {
+    const { post } = props;
+    const [user] = useAuthState(auth);
 
-      const addLike= async()=>{
-    // console.log(data);
-    await addDoc(LikesRef,{userId:user?.uid , postid:post.id});
-//    navigate("/");
-    
-      };
-    return <div>hi from Posts
+    const addLike = async () => {
+        if (!user) {
+            alert("You need to be logged in to like a post.");
+            return;
+        }
 
-        <div className="title">
-            <h1 className="title">{post.title}</h1>
+        try {
+            const LikesRef = collection(db, "likes");
+            await addDoc(LikesRef, { userId: user.uid, postId: post.id });
+            console.log("Like added successfully!");
+        } catch (error) {
+            console.error("Error adding like:", error);
+        }
+    };
+
+    return (
+        <div>
+            <div className="title">
+                <h1 className="title">{post.title}</h1>
+            </div>
+            <div className="body">
+                <p>{post.description}</p>
+            </div>
+            <div className="footer">
+                <p>@{post.username}</p>
+                <button onClick={addLike}>👍</button>
+            </div>
         </div>
-        <div className="body">
-            <p>
-                {post.description}
-            </p>
-        </div>
-        <div className="footer">
-            <p>@{post.username}</p>
-            <button onClick={addLike}>    &#128077; </button>
-        </div>
-    </div>
-}
+    );
+};
