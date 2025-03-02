9f672ee15293232c37102a8f5f0da61382984828
Nikhil Hazari <nikhilhazari100@gmail.com>
2025-03-02 23:24:10 +0530

    trying like feature

--- src/Pages/Main/Post.tsx
+++ src/Pages/Main/Post.tsx
@@ -1,4 +1,7 @@
+import { addDoc, collection } from "firebase/firestore";
 import { Post as IPost } from "./Main"
+import { auth, db } from "../../config/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 interface Props{
     post: IPost;
@@ -6,6 +9,15 @@ interface Props{
 
 export const Post =(props:Props)=>{
     const {post}=props;
+    const [user]=useAuthState(auth);
+      const LikesRef = collection(db,"likes")
+
+      const addLike= async()=>{
+    // console.log(data);
+    await addDoc(LikesRef,{userId:user?.uid , postid:post.id});
+//    navigate("/");
+    
+      };
     return <div>hi from Posts
 
         <div className="title">
@@ -18,6 +30,7 @@ export const Post =(props:Props)=>{
         </div>
         <div className="footer">
             <p>@{post.username}</p>
+            <button onClick={addLike}>    &#128077; </button>
         </div>
     </div>
 }
